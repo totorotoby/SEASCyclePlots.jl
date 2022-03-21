a337af6f333fe0d1f002fceaa45f1f72c9d94e4b
Documenter.jl <documenter@juliadocs.github.io>
2022-03-21 16:15:57 +0000

    build based on b115bcd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SEASCyclePlots","category":"page"},{"location":"#SEASCyclePlots","page":"Home","title":"SEASCyclePlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SEASCyclePlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SEASCyclePlots]","category":"page"}]
+}

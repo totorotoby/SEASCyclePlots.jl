bce34414d0283361287d3814051f34aa610f85e5
Toby Harvey <tharvey2@uoregon.edu>
2022-04-21 12:17:55 -0400

    trying to figure out modules

--- src/SEASCyclePlots.jl
+++ src/SEASCyclePlots.jl
@@ -1,19 +1,35 @@
 module SEASCyclePlots
 
+module io
+
 using NCDatasets
 using Printf
 using DelimitedFiles
-using GLMakie
-using Makie
 using Interpolations
 using IterTools
-
-const year_seconds = 31556952
-const vars_name = ("δ", "V", "τ", "ψ")
 
 include("conversions.jl")
 include("utils.jl")
 include("io.jl")
+
+end
+
+module plots
+
+const year_seconds = 31556952
+const vars_name = ("δ", "V", "τ", "ψ")
+
+using NCDatasets
+using Printf
+using DelimitedFiles
+using Interpolations
+using IterTools
+using Makie
+using GLMakie
+
 include("plots.jl")
 
 end
+
+end
+
